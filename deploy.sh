1dcdaf67a9b2ea920bda5d7f029992e70109887b
tolludare <tolludare@gmail.com>
2023-02-02 18:06:44 +0000

    conflict 2 resolved

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+deploy java application
+Deploy python
+deploy in the dev env
+deployment4
+deploy in to staging environment
+echo good job
+Tested and accepted
+tested and approved by the client QA team
